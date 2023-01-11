b2ff099bae2413b3d18bd51f1b775949982c3398
Jhonatan Rodrigues <jhonatan07tr@gmail.com>
2023-01-10 22:56:53 -0300

    update

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,33 +9,32 @@ public class Program {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		Set<Integer>list = new HashSet<>();
+		Set<Integer> list = new HashSet<>();
 
 		System.out.print("How many Students in the course A? ");
 		int n = sc.nextInt();
 		for (int i = 0; i < n; i++) {
 			int id = sc.nextInt();
-			list.add(id);	
-			
+			list.add(id);
+
 		}
-		
+
 		System.out.print("How many Students in the course B? ");
 		n = sc.nextInt();
 		for (int i = 0; i < n; i++) {
 			int id = sc.nextInt();
-			list.add(id);	
-			
+			list.add(id);
+
 		}
 		System.out.print("How many Students in the course C? ");
 		n = sc.nextInt();
 		for (int i = 0; i < n; i++) {
 			int id = sc.nextInt();
-			list.add(id);	
-			
-		}
-		
-		System.out.println("Total Students: " + list.size());
+			list.add(id);
 
+		}
+
+		System.out.println("Total Students: " + list.size());
 
 		sc.close();
 
